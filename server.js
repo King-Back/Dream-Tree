bbf3de5b3f1334da32a9c12cadbb39d226590022
goddessting <1766153116@qq.com>
2017-04-18 21:18:29 +0800

    change some files

--- server.js
+++ server.js
@@ -8,6 +8,37 @@ app.get("*", function (req, res) {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
+var bodyParser = require('body-parser');
+const cors = require('cors');
+
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/Time-in-place');
+
+var db = mongoose.connection;
+db.on('error', console.log);
+db.once('open', function () {
+    console.log('success!');
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(express.static('./', {
+    dotfiles: 'ignore',
+    etag: true,
+    extensions: ['html', 'js', 'css'],
+    index: ['index.html'],
+    maxAge: '1d',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+        res.set('x-timestamp', Date.now());
+    }
+}));
+
 app.listen(3000, () => {
     console.log('server start');
 });
+
