ecee02cd8d95020ab34eec9c56caf69e4bd1dcae
zjjxj <592670776@qq.com>
2017-05-20 16:10:08 +0800

    finish edit-problem function

--- public/js/components/edit-problem.js
+++ public/js/components/edit-problem.js
@@ -6,15 +6,16 @@ export default class EditProblem extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            title: '',
-            description: '',
-            author: '',
+            title: null,
+            description: null,
+            author: null,
             flag: true
         }
     }
 
     componentWillMount() {
         this.props.overDisplayProblem();
+
     }
 
     onchangeTitle() {
@@ -37,22 +38,24 @@ export default class EditProblem extends React.Component {
 
     updateProblem() {
         const title = this.refs.title.value;
-        const content = this.refs.description.value;
-        const author = this.props.username;
+        const description = this.refs.description.value;
+        const author = this.refs.author.value;
         const id = this.props.id;
 
-        if (!title || !content || !author) {
+        if (!title || !description ) {
+
             alert("发布问题时所有项不能为空!");
             return;
         }
 
-        this.props.onUpdateProblem({id, title, content, author});
+        this.props.onUpdateProblem({id, title, description, author});
     }
 
     render() {
-        const title = this.state.title;
-        const description = this.state.description;
-        const author = this.state.author;
+        const title= this.state.title ===null ? this.props.title:this.state.title;
+        const description= this.state.description ===null ? this.props.description:this.state.description;
+        const author= this.props.author;
+
 
         return <div className="col-md-8 col-md-offset-2">
             <div className="titlePage">
@@ -63,7 +66,7 @@ export default class EditProblem extends React.Component {
                     <label className="col-md-2 control-label">标题</label>
                     <div className="col-md-4">
                         <input type="text" className="form-control" ref="title"
-                               value={!title ? this.props.title : title}
+                               value={title}
                                onChange={this.onchangeTitle.bind(this)}/>
                     </div>
                 </div>
@@ -71,7 +74,7 @@ export default class EditProblem extends React.Component {
                     <label className="col-md-2 control-label">详细描述</label>
                     <div className="col-md-8">
                         <textarea rows="10" className="form-control" ref="description"
-                                  value={!description ? this.props.description : description}
+                                  value={description}
                         onChange={this.onchangeDescription.bind(this)}/>
                     </div>
                 </div>
@@ -79,13 +82,13 @@ export default class EditProblem extends React.Component {
                     <label className="col-md-2 control-label">发布人</label>
                     <div className="col-md-4">
                         <input type="text" className="form-control" ref="author"
-                               value={!author ? this.props.author : author} onChange={this.onchangeAuthor.bind(this)}/>
+                               value={ author} />
                     </div>
                 </div>
             </form>
             <div>
                 <button className="btn btn-primary col-md-2 col-md-offset-5 submitButton"
-                        onclick={this.updateProblem.bind(this)}>更新
+                        onClick={this.updateProblem.bind(this)}>更新
                 </button>
             </div>
         </div>;
--- server/routers/update-problem.js
+++ server/routers/update-problem.js
@@ -1,11 +1,11 @@
 const Problem = require('./../dbs/Model/problem');
 
 exports.updateProblem = function (req, res) {
-    Problem.update({_id: req.problem.id}, {
+    Problem.update({_id: req.body.id}, {
         $set: {
-            title: req.problem.title,
-            description: req.problem.description,
-            author: req.problem.author
+            title: req.body.title,
+            description: req.body.description,
+            author: req.body.author
         }
     }).sort({'updatedAt': -1}).exec(function (err, info) {
         if (err) return res.status(500).json({error: err.message});
